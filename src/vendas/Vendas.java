335e5cfb5d096c2fda970dc3e48754db1a458487
luiz <luiz@luiz-desktop>
2019-10-30 20:30:54 -0400

    TESTANDO

--- src/vendas/Vendas.java
+++ src/vendas/Vendas.java
@@ -16,6 +16,9 @@ public class Vendas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        System.out.println("TESTANDO");
     }
     
 }
